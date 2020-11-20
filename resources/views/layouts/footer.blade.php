7e794eb2b311d547a7262ab603a6e86044fefc7f
ABM-code <abm319@gmail.com>
2020-11-20 19:28:04 +0200

    upload Project

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <div class="container-fluid">
+
+        <div class="copyright">
+            &copy; {{ now()->year }} {{ __('made with') }} <i class="tim-icons icon-heart-2"></i> {{ __('by ABM') }}
+            <a href="https://facebook.com" target="_blank">{{ __('Facebook') }}</a> &amp;
+            <a href="https://twitter.com" target="_blank">{{ __('twitter') }}</a> {{ __('for a better web') }}.
+        </div>
+    </div>
+</footer>
